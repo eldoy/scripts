3daed799ea808db633ce4b6d6b5e314035634da3
Vidar <vidar@fugroup.net>
2017-06-13 19:34:37 +0200

    Fixed deploy

--- lib/rerun.rb
+++ lib/rerun.rb
@@ -18,17 +18,3 @@ def testrun(args)
     puts "Shutting down..."; exit(0)
   end
 end
-
-# Kill the process running at port. Usage kp <port>
-def kp(args)
-  args.each do |port|
-    r = `lsof -i tcp:#{port}`
-    r.split("\n").each do |l|
-      if l =~ /^ruby/
-        l =~ /[A-z]+\s+(\d{1,10})/
-        pid = $1
-        `kill -1 #{pid}` if pid
-      end
-    end
-  end
-end
